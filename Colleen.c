74e995d71b48fe75d88cc1c9b66826142038a3c6
Gordon RASS-KWASI <grass-kw@e1r3p16.42.fr>
2017-03-25 19:15:48 +0100

    Colleen en cours

---
+++ Colleen.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+// hello programme return
+
+int hello(){int i = 32 + 8;return (i);}
+int main() {/* hello world ! */
+char *s="#include <stdio.h>%c// this programme replicate him self%c/* hello world ! */%cint main() {/* hello world ! */%cchar *s=%c%s%c;printf(s, 10, 10, 10, 34, s, 34, 10);return (0);}%c";printf(s, 10, 10, 10, 34, s, 34, 10);return (0);}
