90ce9600e07a2157e5d9f7f08ac05467c4b5c7e5
Gordon RASS-KWASI <grass-kw@e2r2p3.42.fr>
2018-05-10 18:40:01 +0200

    sully en cours

--- Sully.c
+++ Sully.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define PROGRAM "#include <stdio.h>%1$c#include <stdlib.h%1$c%1$c#define PROGRAM %3$c%5$s%3$c%1$cvoid create_file(int i) {char s[40];sprintf(s, %3$c Sully_%d.c%3$c, i);"
+#define PROGRAM "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$c#define PROGRAM %3$c%5$s%3$c%1$cvoid create_file(int i) {char s[40];sprintf(s, %3$cSully_%d.c\", i);FILE *f = fopen(s, \"w\");char s1[70];sprintf(s1, \"gcc -Wall -Werror -Wextra Sully_%1$d;./Sully_%1$d%3$c, i);char *n = PROGRAM;"
 void create_file(int i) {
 char s[40];
 sprintf(s, "Sully_%d.c", i);
--- Sully_5.c
+++ Sully_5.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h
+#include <stdlib.h>
 
-#define PROGRAM "#include <stdio.h>%1$c#include <stdlib.h%1$c%1$c#define PROGRAM %3$c%5$s%3$c%1$cvoid create_file(int i) {char s[40];sprintf(s, %3$cSully_%d.c%3$c, i);"
-void create_file(int i) {char s[40];sprintf(s, "Sully_5.c", i);
+#define PROGRAM "#include <stdio.h>%1$c#include <stdlib.h>%1$c%1$c#define PROGRAM %3$c%5$s%3$c%1$cvoid create_file(int i) {char s[40];sprintf(s, %3$cSully_%d.c", i);FILE *f = fopen(s, "w");char s1[70];sprintf(s1, "gcc -Wall -Werror -Wextra Sully_%1$d;./Sully_%1$d%3$c, i);char *n = PROGRAM;"
+void create_file(int i) {char s[40];sprintf(s, "Sully_5.c", i);FILE *f = fopen(s, "w");char s1[70];sprintf(s1, "gcc -Wall -Werror -Wextra Sully_10;./Sully_10", i);char *n = PROGRAM;
